c174a95ce504e1a9bd707ecbd6773d8d7b6fb310
PujBan <bansari.pujara@simformsolutions.com>
2023-05-17 19:10:58 +0530

    commit 2.1

--- cypress/Integration/Examples/Login.js
+++ cypress/Integration/Examples/Login.js
@@ -1,16 +1,27 @@
 /// <reference types = 'cypress'/>
 
 import login from "../POM/login"
+import products from "../POM/Productspom"
 
 describe('Test Suite', ()=>{
-
-    it('Login Test case', ()=>{
-
+    const logs = []
+    it.only('Login Test case', ()=>{
         const lp = new login()
         lp.visit()
+        cy.wait(5000)
         lp.getUserName('standard_user')
         lp.getpassword('secret_sauce')
         lp.login()
+        cy.screenshot()
+        cy.writeFile('logs.txt', logs)
+        
+    })
+    it('Products Test case', ()=>{
+
+        const pd = new products()
+        pd.filter()
+        pd.addToCart()
+        pd.goToCart()
         cy.screenshot()
 
     })
--- cypress/Integration/POM/Productspom.js
+++ cypress/Integration/POM/Productspom.js
@@ -1,17 +1,18 @@
 class products{
 
     filter(){
+        cy.wait(60000)
         cy.get('#header_container > div.header_secondary_container > div > span > select').select(1).invoke("val").should("eq","za")
     }
     addToCart(){
 
-       price = cy.get('#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > div')
+    const price = cy.get('div[class=inventory_list] div:nth-child(1) div:nth-child(2) div:nth-child(2) div:nth-child(1)')
        cy.log(price)
        const addtocart =  cy.get('#add-to-cart-test\.allthethings\(\)-t-shirt-\(red\)')
        addtocart.click()
        cy.get('#remove-test\.allthethings\(\)-t-shirt-\(red\)').should('have.text', 'Remove')
 
-       price1 = cy.get('#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > div')
+       const price1 = cy.get('#inventory_container > div > div:nth-child(1) > div.inventory_item_description > div.pricebar > div')
        cy.log(price1)
        const addtocart1 = cy.get('#add-to-cart-sauce-labs-onesie')
        addtocart1.click()
@@ -24,3 +25,4 @@ class products{
 
     }
 }
+export default products
